efee5cf6a1e3a989703872a44e632ef93f12fc9f
Alexandre R. Silva <alexandre9387@gmail.com>
2024-09-16 19:24:46 -0300

    Commit Vercel

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript code can be added here for future interactivity
+    console.log('Página carregada com sucesso!');
+});
